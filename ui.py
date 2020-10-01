bd916614752c3d5d7182b2925ee76e391f0b5465
john jack <jklewis99@gmail.com>
2020-10-01 10:57:29 -0500

    failing code

--- ui.py
+++ ui.py
@@ -4,12 +4,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QDir, Qt, QUrl
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtWidgets import (QApplication, QFileDialog, QHBoxLayout, QLabel,
+from PyQt5.QtWidgets import (QFileDialog, QGridLayout, QHBoxLayout, QLabel,
         QPushButton, QSizePolicy, QSlider, QStyle, QVBoxLayout, QWidget)
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QAction
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton
+from PyQt5.QtGui import QPixmap, QImage
 import sys
-
+import cv2
 
 class Ui_MainWindow(QMainWindow):
 
@@ -20,6 +20,7 @@ class Ui_MainWindow(QMainWindow):
         self.setup_media()
 
     def setup_ui(self):
+        # basically all the buttons in the menu bar
         self.menubar = QtWidgets.QMenuBar(self)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 1255, 25))
         self.menubar.setObjectName("menubar")
@@ -158,9 +159,10 @@ class Ui_MainWindow(QMainWindow):
                 QSizePolicy.Maximum)  
 
     def setup_media(self):
+        # setup for the video playback
         self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
 
-        videoWidget = QVideoWidget()
+        project_video_widget = QVideoWidget()
         
         self.init_buttons()
         self.init_sliders()
@@ -169,28 +171,52 @@ class Ui_MainWindow(QMainWindow):
         # Create a widget for window contents
         wid = QWidget(self)
         self.setCentralWidget(wid)
+        grid = QGridLayout()
+        wid.setLayout(grid)
 
         # Create layouts to place inside widget
         controlLayout = QHBoxLayout()
-        controlLayout.setContentsMargins(0, 0, 0, 0)
+        # controlLayout.setContentsMargins(0, 0, 0, 0)
         controlLayout.addWidget(self.playButton)
         controlLayout.addWidget(self.positionSlider)
 
         # 2 video panels??
-        tempWidget = QVideoWidget()
-        video_layout = QHBoxLayout()
-        video_layout.addWidget(tempWidget)
-        video_layout.addWidget(videoWidget)
+        clip_video_widget = QLabel()
+        clip_video_widget.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
+        clip_video_widget.resize(1000, 1000)
+        # clip_video_widget.setAlignment(Qt.AlignCenter)
+        # clip_video_widget.setPixmap(QtGui.QPixmap("C:/Users/jklew/OneDrive/Pictures/madmax.jpg"))
+        # clip_video_widget.setScaledContents(True)
+        im_np = cv2.imread("C:/Users/jklew/OneDrive/Pictures/madmax.jpg")
+        clip_frame = QImage(im_np, im_np.shape[1], im_np.shape[0], QImage.Format_RGB888)
+        clip_pixmap = QPixmap(clip_frame)
+        clip_pixmap = clip_pixmap.scaled(1000, 1000, Qt.KeepAspectRatio)
+        clip_video_widget.setPixmap(clip_pixmap)
+        clip_video_widget.setMinimumWidth(700) # only way to see it on window?
+        # clip_video_widget.setMaximumSize(1920//2, 1080//2)
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(clip_video_widget)
+        # left_layout.addLayout(controlLayout)
 
-        layout = QVBoxLayout()
-        layout.addLayout(video_layout)
-        layout.addLayout(controlLayout)
-        layout.addWidget(self.errorLabel)
+        right_layout = QVBoxLayout()
+        # right_layout.addWidget(clip_video_widget)
+        right_layout.addWidget(project_video_widget)
+        right_layout.addLayout(controlLayout)
+        right_layout.addWidget(self.errorLabel)
+
+        # layout = QHBoxLayout()
+        # layout.addLayout(left_layout)
+        # layout.addLayout(right_layout)
+        
+        
 
         # Set widget to contain window contents
-        wid.setLayout(layout)
+        # grid.addWidget(clip_video_widget, 0, 0)
+        grid.addLayout(left_layout, 0, 0)
+        grid.addLayout(right_layout, 0, 1)
+        # wid.setLayout(layout)
 
-        self.mediaPlayer.setVideoOutput(videoWidget)
+        self.mediaPlayer.setVideoOutput(project_video_widget)
         self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
         self.mediaPlayer.positionChanged.connect(self.positionChanged)
         self.mediaPlayer.durationChanged.connect(self.durationChanged)
